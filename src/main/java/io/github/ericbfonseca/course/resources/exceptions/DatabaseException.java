5f1b0d6c293931d7b14c9ab2705c63e7d67465b5
Eric Fonseca <ericbfonseca@gmail.com>
2025-02-03 16:49:39 -0300

    Exception handling - delete & update

---
+++ src/main/java/io/github/ericbfonseca/course/resources/exceptions/DatabaseException.java
@@ -0,0 +1,11 @@
+package io.github.ericbfonseca.course.resources.exceptions;
+
+public class DatabaseException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+	
+}
